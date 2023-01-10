57b214a72bbc79c7ab2b145d2dce10578f35d0bd
paulalbert1 <paa2013@med.cornell.edu>
2023-01-10 14:14:32 -0500

    Update setupReciterDB.py

--- setup/setupReciterDB.py
+++ setup/setupReciterDB.py
@@ -57,8 +57,11 @@ def main():
 
     databaseConnection = connect_mysql_server(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)  
     run_sql_file("createDatabaseTableReciterDb.sql", databaseConnection)     
+'''  
+Commenting these out with the expectation that they would be imported manually.  
     run_sql_file("insertBaselineDataReciterDb.sql", databaseConnection)     
     run_sql_file("createEventsProceduresReciterDb.sql", databaseConnection) 
+'''
     databaseConnection.close() 
      
 if __name__ == "__main__": 
