87130445dadab7375159c8eab09ab071c25d9a3a
paulalbert1 <paa2013@med.cornell.edu>
2023-08-01 09:05:57 -0400

    NIH attribute is_research_article is case sensitive

--- setup/createEventsProceduresReciterDb.sql
+++ setup/createEventsProceduresReciterDb.sql
@@ -1875,7 +1875,7 @@ BEGIN
 ## 2. analysis_summary_article - article-level data
 ## 3. analysis_summary_person - person-level data
 
-## In WCM's experience, this procedure takes ~11 minutes to complete while running on AWS 
+## In WCM's experience, this procedure takes ~17 minutes to complete while running on AWS RDS, MariaDB 10.6
 ## and with about 300,000 accepted articles. 
 
 ## The below is a hedge just to make sure this procedure doesn't start 
@@ -2042,7 +2042,7 @@ set a.citationCountNIH = r.citation_count,
 a.percentileNIH = r.nih_percentile,
 a.relativeCitationRatioNIH = r.relative_citation_ratio,
 publicationTypeNIH = 
-  case when is_research_article = 'yes' then 'Research Article'
+  case when is_research_article in ('Yes','yes') then 'Research Article'
   else null
   end;
 
@@ -2826,6 +2826,17 @@ where a.userID  not in
 (select userID
 from admin_users_roles
 where roleID = 4);
+
+-- Let's give self-curators the ability to generate reports for everyone
+
+insert into admin_users_roles (userID, roleID)
+select a.userID, 3 
+from person p 
+join admin_users a on a.personIdentifier = p.personIdentifier
+left join (select userID
+from admin_users_roles
+where roleID = 3) x on x.userID = a.userID
+where x.userID is null;
 
 
 END;
