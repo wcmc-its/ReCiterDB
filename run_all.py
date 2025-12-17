5e1b7fbd7a63f352ac88826eb8361ebd08acf57d
mrj4001 <mrj4001@med.cornell.edu>
2025-12-17 13:10:42 +0530

    added all the scripts back to the cronjob

--- run_all.py
+++ run_all.py
@@ -110,14 +110,11 @@ def upload_log_to_s3():
 # ------------- Main Flow -------------
 def main():
     scripts = [
-        #("executeFeatureGenerator", "python3 executeFeatureGenerator.py"),
-        #("retrieveS3", "python3 retrieveS3.py"),
-        #("retrieveDynamoDb", "python3 retrieveDynamoDb.py"),
-        #("updateReciterDB", "python3 updateReciterDB.py"),
-       # ("retrieveArticles", "python3 retrieveArticles.py"),
-       # ("retrieveNIH", "python3 retrieveNIH.py"),  
-       # ("conflictsImport", "python3 conflictsImport.py"),
-        #("abstractImport", "python3 abstractImport.py")
+        ("executeFeatureGenerator", "python3 executeFeatureGenerator.py"),
+        ("retrieveArticles", "python3 retrieveArticles.py"),
+        ("retrieveNIH", "python3 retrieveNIH.py"),  
+        ("conflictsImport", "python3 conflictsImport.py"),
+        ("abstractImport", "python3 abstractImport.py")
         ("nightlyIndexing", "bash run_nightly_indexing.sh"),
     ]
 
